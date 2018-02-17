e0db01ffc058e987d445c20695485aab69677575
akbarmehdiloo <36171916+akbarmehdiloo@users.noreply.github.com>
2018-02-17 20:36:25 +0000

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,1 +1,9 @@
+# Akbar Mehdiloo 
 
+number = input("Please any number you wish: ")
+while number != 1: 
+    if number % 2 == 0:
+        print(number // 2)
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
