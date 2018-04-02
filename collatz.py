d43403101a2654ddfa21fdd750587a798c8c8bbc
akbarmehdiloo <36171916+akbarmehdiloo@users.noreply.github.com>
2018-04-02 10:16:56 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,9 +1,13 @@
 # Akbar Mehdiloo 
 
-number = input("Please any number you wish: ")
-while number != 1: 
+n = input("Please enter a number ")
+def collatz(number):
     if number % 2 == 0:
         print(number // 2)
+        return number // 2
     elif number % 2 == 1:
         result = 3 * number + 1
         print(result)
+        return result
+while n != 1:
+    n = collatz(int(n))
