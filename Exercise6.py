de6084706b91070cada290c4d4a511c66b1fcdf7
akbarmehdiloo <36171916+akbarmehdiloo@users.noreply.github.com>
2018-04-02 10:33:31 +0100

    Update Exercise6.py

--- Exercise6.py
+++ Exercise6.py
@@ -5,6 +5,6 @@ def factorial(number):
     for i in range(1,number+1):
         b=b*i 
     return b  
-print('The vactorial of number 5 is:',factorial(5))
-print('The vactorial of number 7 is:',factorial(7))
-print('The vactorial of number 10 is:',factorial(10))
+print('The vactorial of number 5 is:',factorial(5))   # answer = 120
+print('The vactorial of number 7 is:',factorial(7))   # answer = 5040 
+print('The vactorial of number 10 is:',factorial(10))  # answer = 3628800
