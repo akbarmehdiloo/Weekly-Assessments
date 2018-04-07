3e53c3c3beb5788ddfeb30ac9c6134e778fe8085
akbarmehdiloo <36171916+akbarmehdiloo@users.noreply.github.com>
2018-04-07 20:06:19 +0100

    Update Exercise5.py

--- Exercise5.py
+++ Exercise5.py
@@ -1,8 +1,9 @@
 #Akbar Mehdiloo
 
-import pandas as pd
-iris= pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
-iris_cols=['Petal Length', 'Petal Width', 'Sepal Length','Sepal Width','e']
-iris.columns = iris_cols
-iris.drop('e',axis=1,inplace=True)
+import pandas as pd     #Pandas is a Python module,
+iris= pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')   # This is where I am downloading the dataset using read_csv
+iris_cols=['Petal Length', 'Petal Width', 'Sepal Length','Sepal Width','Class']  # created new variable called iris_cols
+iris.columns = iris_cols    # each of the column header will be equal to iris_cols
+iris.drop('Class',axis=1,inplace=True)   # This will ignor the Class data series as we are intrested on numerical values
 print(iris)
+
